9bfbcb01ac91af0e2f9fc9e213403b7ebcc66e61
nathanielpierson <nathanielepierson@gmail.com>
2025-02-15 19:19:22 -0500

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { SongsPage } from './SongsPage'
+import { Header } from './Header'
 // import { useState } from 'react'
 
 function App() {
 
   return (
     <div>
+      <Header />
       <SongsPage />
       <p>h</p>
     </div>
---
+++ src/Header.jsx
@@ -0,0 +1,3 @@
+export function Header () {
+return <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/2560px-Lastfm_logo.svg.png" width={250} height={70} />
+}
