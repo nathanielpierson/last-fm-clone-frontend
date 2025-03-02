c55796929fbc5ccd7ead38aaa46eb868b1671732
nathanielpierson <nathanielepierson@gmail.com>
2025-03-02 15:47:42 -0500

    add HomePage

---
+++ src/HomePage.jsx
@@ -0,0 +1,11 @@
+import { SongsIndex } from "./SongsIndex";
+import { AlbumsIndex } from "./AlbumsIndex";
+
+export function HomeIndex() {
+  return (
+    <div>
+      <SongsIndex />
+      <AlbumsIndex />
+    </div>
+  );
+}
